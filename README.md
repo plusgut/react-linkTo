0569cc27cafdd5a335a362fe90d99d1a04983e84
plusgut <github@webentwickler2-0.de>
2017-03-17 20:32:53 +0100

    [link] implemented modifier logic

---
+++ README.md
@@ -0,0 +1,12 @@
+React-LinkTo
+===
+
+This is a simple component for generating an a-tag.
+The given onClick property will only be executed, when no modifier key is pressed.
+Else the normal browser behaviour will be done.
+
+As modifier-keys are used ctrl, shift, alt and the mac-cmd
+
+  <LinkTo href="/path/to/something" onClick={this.doSomethingElse}>
+    Text to click
+  </LinkTo>
---
+++ src/LinkTo.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class LinkTo extends Component {
+  /**
+   * @param {*} event
+   * when no modifier, the normal browser-behaviour should be stopped and custom logic executed
+   */
+  handleClick(event) {
+    if (this.hasModifier(event) === false) {
+      event.preventDefault();
+      this.triggerCallback(event);
+    }
+  }
+
+  /**
+   * @param {*} event
+   * checks if modifier-key is active
+   */
+  hasModifier(event) {
+    return event.ctrlKey || event.shiftKey || event.altKey || event.metaKey;
+  }
+
+  /**
+   * overwriting this function may be useful, for making history.pushState
+   */
+  triggerCallback() {
+    this.props.onClick(event);
+  }
+
+  render() {
+    return (
+      <a {...this.props} onClick={this.handleClick.bind(this)}>
+        {this.props.children}
+      </a>
+    );
+  }
+}
+
+export default LinkTo;
